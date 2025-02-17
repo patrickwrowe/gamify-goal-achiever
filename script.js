62741d138556909348e2b75d69085529afc9e20e
Patrick Rowe <patrick.william.rowe@gmail.com>
2025-02-17 22:04:15 +0000

    reorder file to test text prints to console

--- script.js
+++ script.js
@@ -1,44 +1,5 @@
 //import OpenAI from "openai";
 
-// Get input element and button 
-const input = document.getElementById('goal-text');
-const button = document.getElementById('get-goals');
-
-// Add event listener to button
-button.addEventListener('click', getGoals);
-
-// Create a new instance of the OpenAI class
-const openai = new OpenAI();
-
-const gptPromptTemplateV1 = "Provide a list of 20 suggestions for milestones for an individual who wants to achieve the following goal: ${goal}. You are to provide these so that they can be completed with a range of time requirements, from a few hours up to a few weeks. Where possible, quests should build on top of previous, more easily attainable quests, to provide a feeling of achievement for making progress in a few specific areas. You should return your results in the json format. You should only provide the json. You should not provide any additional text."
-
-function getGoals() {
-    console.log("Button clicked");
-    const goalTextClean = sanitizeInput(input.value);
-    console.log(goalTextClean);
-
-    console.log("here")
-    console.log(exampleOutput[0].id);
-    console.log(exampleOutput[0].milestone);
-    console.log(exampleOutput[0].description);
-}
-
-function sanitizeInput(input) {
-    return input.replace(/[^a-zA-Z0-9 ]/g, '');
-}
-
-/*
-const completion = await openai.chat.completions.create({
-    model: "gpt-4o-mini",
-    store: true,
-    messages: [
-        {"role": "user", "content": `${GptPrompt}`}
-    ]
-});    
-
-console.log(completion.data.choices[0].message.content);
-*/
-
 const exampleOutput = [
     {
       "id": 1,
@@ -160,4 +121,43 @@ const exampleOutput = [
       "description": "After building up your success over several weeks, reward yourself for a month without smoking by celebrating in a meaningful way and setting new health goals.",
       "time_required": "a few weeks"
     }
-  ]
+]
+
+// Get input element and button 
+const input = document.getElementById('goal-text');
+const button = document.getElementById('get-goals');
+
+// Add event listener to button
+button.addEventListener('click', getGoals);
+
+// Create a new instance of the OpenAI class
+//const openai = new OpenAI();
+
+const gptPromptTemplateV1 = "Provide a list of 20 suggestions for milestones for an individual who wants to achieve the following goal: ${goal}. You are to provide these so that they can be completed with a range of time requirements, from a few hours up to a few weeks. Where possible, quests should build on top of previous, more easily attainable quests, to provide a feeling of achievement for making progress in a few specific areas. You should return your results in the json format. You should only provide the json. You should not provide any additional text."
+
+function getGoals() {
+    console.log("Button clicked");
+    const goalTextClean = sanitizeInput(input.value);
+    console.log(goalTextClean);
+
+    console.log("here")
+    console.log(exampleOutput[0].id);
+    console.log(exampleOutput[0].milestone);
+    console.log(exampleOutput[0].description);
+}
+
+function sanitizeInput(input) {
+    return input.replace(/[^a-zA-Z0-9 ]/g, '');
+}
+
+/*
+const completion = await openai.chat.completions.create({
+    model: "gpt-4o-mini",
+    store: true,
+    messages: [
+        {"role": "user", "content": `${GptPrompt}`}
+    ]
+});    
+
+console.log(completion.data.choices[0].message.content);
+*/
